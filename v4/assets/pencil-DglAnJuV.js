b132c83e1cb57aba5a4c0343d3c05b659deab8c6
AbdallahElSadek21 <projects1@luxshtech.com>
2026-09-04 11:03:42 +0100

    V.4 preview refreshed to 8a85410 - QA/Super Admin password dialogs, six-row Home panels, sample-email buttons gone, POD tap-to-scan and a fuller collection round

--- v4/assets/pencil-DglAnJuV.js
+++ v4/assets/pencil-DglAnJuV.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-XPPi0x-g.js";/**
+import{c as a}from"./index-DF9lxh5u.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- v4/assets/shopping-cart-CEB56Hbm.js
+++ v4/assets/shopping-cart-CEB56Hbm.js
@@ -1,4 +1,4 @@
-import{c}from"./index-XPPi0x-g.js";/**
+import{c}from"./index-DF9lxh5u.js";/**
  * @license lucide-react v0.487.0 - ISC
  *
  * This source code is licensed under the ISC license.
